2f93b7256a4bdcdac36ed539e2e13f656788312e
augmented-fog <114533816+augmented-fog@users.noreply.github.com>
2024-06-04 15:28:10 -0400

    Removed repository id from partial path structs

--- cozo-core/src/fixed_rule/algos/stack_graph/partial_path_file_info.rs
+++ cozo-core/src/fixed_rule/algos/stack_graph/partial_path_file_info.rs
@@ -3,22 +3,19 @@ use crate::fixed_rule::algos::stack_graph::stack_graph_storage_error::StackGraph
 use crate::fixed_rule::algos::stack_graph::stack_graph_storage_error::StackGraphStorageError::InvalidTuple;
 
 pub struct PartialPathFileInfo {
-    pub repository_id: String,
-    pub blob_id: String,
+    pub file: String,
     pub local_id: u32,
     pub value: Vec<u8>,
 }
 
 impl PartialPathFileInfo {
     pub fn new(
-        repository_id: String,
         blob_id: String,
         local_id: u32,
         value: Vec<u8>,
     ) -> Self {
         Self {
-            repository_id,
-            blob_id,
+            file,
             local_id,
             value,
         }
@@ -29,23 +26,19 @@ impl TryFrom<Tuple> for PartialPathFileInfo {
     type Error = StackGraphStorageError;
 
     fn try_from(tuple: Tuple) -> Result<Self, Self::Error> {
-        if tuple.len() != 4 {
+        if tuple.len() != 3 {
             return Err(InvalidTuple);
         }
 
-        let repository_id = tuple[0].get_str();
-        let blob_id = tuple[1].get_str();
-        let local_id = tuple[2].get_int();
-        let value = tuple[3].get_bytes();
+        let file = tuple[0].get_str();
+        let local_id = tuple[1].get_int();
+        let value = tuple[2].get_bytes();
 
         // TODO: replace unwrap and handle error
         Ok(Self {
-            repository_id: String::from(repository_id.unwrap()),
-            blob_id: String::from(blob_id.unwrap()),
+            file: String::from(file.unwrap()),
             local_id: local_id.unwrap() as u32,
             value: Vec::from(value.unwrap()),
         })
     }
 }
-
-
--- cozo-core/src/fixed_rule/algos/stack_graph/partial_path_root_info.rs
+++ cozo-core/src/fixed_rule/algos/stack_graph/partial_path_root_info.rs
@@ -3,22 +3,19 @@ use crate::fixed_rule::algos::stack_graph::stack_graph_storage_error::StackGraph
 use crate::fixed_rule::algos::stack_graph::stack_graph_storage_error::StackGraphStorageError::InvalidTuple;
 
 pub struct PartialPathRootInfo {
-    pub repository_id: String,
-    pub blob_id: String,
-    symbol_stack: String,
-    value: Vec<u8>,
+    pub file: String,
+    pub symbol_stack: String,
+    pub value: Vec<u8>,
 }
 
 impl PartialPathRootInfo {
     pub fn new(
-        repository_id: String,
-        blob_id: String,
+        file: String,
         symbol_stack: String,
         value: Vec<u8>,
     ) -> Self {
         Self {
-            repository_id,
-            blob_id,
+            file,
             symbol_stack,
             value,
         }
@@ -29,19 +26,17 @@ impl TryFrom<Tuple> for PartialPathRootInfo {
     type Error = StackGraphStorageError;
 
     fn try_from(tuple: Tuple) -> Result<Self, Self::Error> {
-        if tuple.len() != 4 {
+        if tuple.len() != 3 {
             return Err(InvalidTuple);
         }
 
-        let repository_id = tuple[0].get_str();
-        let blob_id = tuple[1].get_str();
-        let symbol_stack = tuple[2].get_str();
-        let value = tuple[3].get_bytes();
+        let file = tuple[0].get_str();
+        let symbol_stack = tuple[1].get_str();
+        let value = tuple[2].get_bytes();
 
         // TODO: replace unwrap and handle error
         Ok(Self {
-            repository_id: String::from(repository_id.unwrap()),
-            blob_id: String::from(blob_id.unwrap()),
+            file: String::from(file.unwrap()),
             symbol_stack: String::from(symbol_stack.unwrap()),
             value: Vec::from(value.unwrap()),
         })
